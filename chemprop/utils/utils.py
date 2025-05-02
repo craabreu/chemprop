7a803ef8d992c1c622cf5b9d8420e45dad16cf96
Charlles Abreu <craabreu@gmail.com>
2025-05-02 16:10:54 -0400

    Implemented relative neighbor ranking

--- chemprop/utils/utils.py
+++ chemprop/utils/utils.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from enum import StrEnum
 from typing import Iterable, Iterator
 
+import numpy as np
 from rdkit import Chem
 
 
@@ -70,6 +71,111 @@ def make_mol(smi: str, keep_h: bool, add_h: bool, ignore_chirality: bool = False
             atom.SetChiralTag(Chem.ChiralType.CHI_UNSPECIFIED)
 
     return mol
+
+
+def is_odd_permutation(i: int, j: int, k: int, m: int | None = None) -> int:
+    r"""Check if a permutation is odd."""
+    swaps = int(i > j) + int(i > k) + int(j > k)
+    if m is not None:
+        swaps += int(i > m) + int(j > m) + int(k > m)
+    return bool(swaps % 2)
+
+
+def set_relative_neighbor_ranking(mol: Chem.Mol, force: bool = False) -> None:
+    r"""Add neighbor ranking information to the bonds of a molecule.
+
+    Neighbors of each atom are sorted in descending order based on their canonical
+    ranks (see :rdmolfiles:`CanonicalRankAtoms`). The descending order keeps hydrogens
+    at the end of the list.
+
+    Two integer properties are added to each bond in the molecule:
+
+    - `endRankFromBegin`: The relative rank of the bond's end atom with respect to all
+        neighbors of the bond's begin atom.
+    - `beginRankFromEnd`: The relative rank of the bond's begin atom with respect to all
+        neighbors of the bond's end atom.
+
+    A relative rank is the position in a sorted list of canonical ranks in descending
+    order. For example, if an atom has three neighbors with canonical ranks `(3, 1, 7)`,
+    their relative ranks are `(1, 2, 0)`. Tied canonical ranks result in the same
+    relative rank, corresponding to the least position they occupy in the descending
+    sorted list.
+
+    The molecule is tagged with a boolean property `hasNeighborRanks` set to True.
+
+    Parameters
+    ----------
+    mol
+        The molecule to add neighbor ranking information to.
+    force
+        Whether to add neighbor ranking information even if it has already been added
+        (default is False).
+
+    Examples
+    --------
+    >>> from rdkit import Chem
+    >>> import numpy as np
+
+    Define a function for printing bond rankings
+
+    >>> def describe_atom(atom):
+    ...     return f"{atom.GetSymbol()}{atom.GetIdx()}"
+    >>> def print_rankings(mol):
+    ...     ranks = {describe_atom(atom): [] for atom in mol.GetAtoms()}
+    ...     for bond in mol.GetBonds():
+    ...         begin = describe_atom(bond.GetBeginAtom())
+    ...         end = describe_atom(bond.GetEndAtom())
+    ...         ranks[begin].append((end, bond.GetIntProp("endRankFromBegin")))
+    ...         ranks[end].append((begin, bond.GetIntProp("beginRankFromEnd")))
+    ...     for atom in ranks:
+    ...         if len(ranks[atom]) > 1:
+    ...             print(atom, *sorted(ranks[atom], key=lambda x: x[1]))
+
+    Assign neighbor ranks to a molecule
+
+    >>> mol = Chem.MolFromSmiles("NC(O)=C(OC)OC")
+    >>> set_relative_neighbor_ranking(mol)
+    >>> print_rankings(mol)
+    C1 ('C3', 0) ('O2', 1) ('N0', 2)
+    C3 ('C1', 0) ('O4', 1) ('O6', 1)
+    O4 ('C3', 0) ('C5', 1)
+    O6 ('C3', 0) ('C7', 1)
+
+    Assign neighbor ranks to molecules with a tetrahedral chiral center
+
+    >>> for smiles in ["C[C@](O)(S)N", "C[C@@](S)(O)N"]:
+    ...     print(f"\nMolecule: {smiles}")
+    ...     mol = Chem.MolFromSmiles(smiles)
+    ...     set_relative_neighbor_ranking(mol)
+    ...     print_rankings(mol)
+    <BLANKLINE>
+    Molecule: C[C@](O)(S)N
+    C1 ('S3', 0) ('O2', 1) ('N4', 2) ('C0', 3)
+    <BLANKLINE>
+    Molecule: C[C@@](S)(O)N
+    C1 ('S2', 0) ('O3', 1) ('N4', 2) ('C0', 3)
+
+    """
+    if not force and mol.HasProp("hasNeighborRanks"):
+        return
+    all_priorities = -np.fromiter(
+        Chem.CanonicalRankAtoms(
+            mol, breakTies=False, includeChirality=False, includeAtomMaps=False
+        ),
+        dtype=int,
+    )
+    sorted_neighbors = []
+    for atom in mol.GetAtoms():
+        neighbors = np.fromiter((atom.GetIdx() for atom in atom.GetNeighbors()), dtype=int)
+        neighbor_priorities = all_priorities[neighbors]
+        ranks = np.searchsorted(np.sort(neighbor_priorities), neighbor_priorities)
+        sorted_neighbors.append(dict(zip(neighbors, ranks)))
+
+    for bond in mol.GetBonds():
+        begin, end = bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()
+        bond.SetIntProp("endRankFromBegin", int(sorted_neighbors[begin][end]))
+        bond.SetIntProp("beginRankFromEnd", int(sorted_neighbors[end][begin]))
+    mol.SetBoolProp("hasNeighborRanks", True)
 
 
 def pretty_shape(shape: Iterable[int]) -> str:
